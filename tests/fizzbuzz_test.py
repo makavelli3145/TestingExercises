79df42575e4d0fb806299b354bdf94f789a517fd
makaveli <casseteinc@gmail.com>
2025-07-13 15:28:56 +0200

    Added more test cases to fizzbuzz_test.py

--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -1,6 +1,8 @@
 from app.fizzbuzz import fizzbuzz
 import pytest
 
-@pytest.mark.parametrize("test_input,expected",[(3, "Fizz"), (5, "Buzz"), (15, "FizzBuzz")])
+@pytest.mark.parametrize("test_input,expected",[(3, "Fizz"), (5, "Buzz"),
+                        (15, "FizzBuzz"),(7, "7"),(30, "FizzBuzz"),(14, "14"),
+                        (23, "23"),(21, "Fizz"),])
 def test_fizzbuzz(test_input, expected):
     assert fizzbuzz(test_input) == expected
